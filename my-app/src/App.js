6d4dfff0e556e5870fd6a267e5605862604c8953
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-15 18:23:49 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SampleClass from './Sample/SampleClass';
+
+function App() {
+  return (
+    <div className="App">
+      <SampleClass></SampleClass>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Sample/SampleClass.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SampleClass extends Component {
+    render() {
+        console.log('SampleClass')
+        
+        // You must need to return something
+        // from component
+        return <span>SAMPLE</span>
+    }
+}
