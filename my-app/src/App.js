d978d5cf4ccc1ccecee7c39bb3757f16fe824c7e
Sanket <sanketjoshi96@gmail.com>
2022-07-11 23:51:36 +0530

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import SimpleReducer from './Sample/SimpleReducer';
-import ComplexReducer from './Sample/ComplexReducer';
+import DataFetchReducer from './Sample/DataFetchReducer';
 
 function App() {
   return (
     <div className="App">
-        <SimpleReducer></SimpleReducer>
-        <ComplexReducer></ComplexReducer>
+        <DataFetchReducer></DataFetchReducer>
     </div>
   );
 }
